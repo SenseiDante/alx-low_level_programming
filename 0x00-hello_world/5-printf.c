f09af7ff3177418a4afb328ca02ee692129145cb
SenseiDante <dnlmusumba@gmail.com>
2023-09-07 16:01:50 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main  - this will display exact text
+ *
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
