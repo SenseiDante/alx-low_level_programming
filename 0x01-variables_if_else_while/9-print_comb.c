f89de51415a9c809a8b7f544cbb0fb3473f170f5
SenseiDante <dnlmusumba@gmail.com>
2023-09-11 14:43:03 -0700

    Persistence

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0 ; n < 10 ; n++)
 	{
-		puchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(n);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
