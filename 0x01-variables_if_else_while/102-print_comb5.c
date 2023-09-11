34ca145058a3914b8a70dcbf10bd66d9d591465b
SenseiDante <dnlmusumba@gmail.com>
2023-09-11 16:02:36 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-        int n, m;
+	int n, m;
 
-        for (n = 0; n < 100; n++)
-        {
-                for (m = 0; m < 100; m++)
-                {
-                        if (n <m)
-                        {       
-				puchar((n / 10) + 48);
-				puchar((n / 10) + 48);
-                                puchar(' ');
-				puchar((m / 10) + 48);
-				puchar((m / 10) + 48);
-                                if (n != 98 || m ! = 99)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (n = 0; n < 100; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (n < m)
+			{
+				putchar((n / 10) + 48);
+				putchar((n / 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m / 10) + 48);
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
