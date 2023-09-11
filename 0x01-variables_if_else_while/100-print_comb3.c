61bfe111bb6c86687ed287156ba27daf64415265
SenseiDante <dnlmusumba@gmail.com>
2023-09-11 15:17:47 -0700

    Combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,23 @@
  */
 int main(void)
 {
-        int n, m;
+	int n, m;
 
-        for (n = 48; n < 56; n++)
-        {
-		for (m = 49; m < 57; m++)
+	for (n = 48 ; n < 56 ; n++)
+	{
+		for (m = 49 ; m < 57 ; m++)
 		{
-			if (m > n)	
-			{	puchar(n);
-				puchar(m);
-                		if (n != 56 || m ! = 57)
-                		{	
-                        		putchar(',');
-                        		putchar(' ');
-                		}
-        		}
+			if (m > n)
+			{	putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
-
