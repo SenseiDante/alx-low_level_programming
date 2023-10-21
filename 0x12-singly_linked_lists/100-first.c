0e79d782fedf5c255dcdbe44b0239db0867af9b4
SenseiDante <dnlmusumba@gmail.com>
2023-10-20 17:38:20 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void first(void)__attribute__((constructor));
+
+/**
+ * first - prints input befor main function is executed
+ *
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
